d75ff10945294571cb623d6a52c6877158b0ab24
Muhammad Isa <muhammad-rk-isa@outlook.com>
2023-06-13 09:33:58 +0600

    connected server to mongodb

--- index.js
+++ index.js
@@ -10,16 +10,8 @@ dotenv.config();
 app.use( express.json() );
 app.use( cors() );
 
-const uri = `mongodb+srv://${ process.env.MDB_USER }:${ process.env.MDB_PASSWORD }@cluster0.9wh3o6k.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient( uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true
-    }
-} );
 
-// ! ------------------------------- JWT Auth Start -----------------------------------
+// ! ================================ JWT Auth Start =================================
 const verifyJWT = ( req, res, next ) => {
     const { authorization } = req.headers;
     const token = authorization.split( ' ' )[ 1 ];
@@ -41,7 +33,48 @@ app.post( '/auth/request_access_token', ( req, res ) => {
 // ! ------------------------------- JWT Auth End -------------------------------------
 
 
-// ! ------------------------------ MongoDB Start -------------------------------------
+
+// ! ============================== MongoDB Start =====================================
+const uri = `mongodb+srv://${ process.env.MDB_USER }:${ process.env.MDB_PASSWORD }@cluster0.9wh3o6k.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient( uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+} );
+
+const run = async () => {
+    try {
+        client.connect();
+
+        // ? Retrieve database and collections
+        const db = client.db( "lingooo" );
+        const classesCollection = db.collection( "classes" );
+        const usersCollection = db.collection( "users" );
+
+        app.get( '/classes', async ( req, res ) => {
+            const { quantity } = req.query;
+            if ( quantity ) {
+                const classes = await classesCollection.find().limit( parseInt( quantity ) ).toArray();
+                res.send( classes );
+            } else {
+                const classes = await classesCollection.find().toArray();
+                res.send( classes );
+            }
+        } );
+
+        // Send a ping to confirm a successful connection
+        await client.db( "admin" ).command( { ping: 1 } );
+        console.log( "Pinged your deployment. You successfully connected to MongoDB!" );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+};
+run().catch( console.dir );
+
+
 
 // ! ------------------------------ MongoDB End ---------------------------------------
 
